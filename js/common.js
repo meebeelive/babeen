6681fcdade8496fe608256ddfb4b085586321979
meebeelive <amittokarim@gmail.com>
2023-07-03 00:23:13 +0400

    php form

--- js/common.js
+++ js/common.js
@@ -499,7 +499,8 @@ function initCursor() {
 		success: 'valid',
 		submitHandler: function() {
 			$.ajax({
-				url: 'mailer/feedback.php',
+				// url: 'mailer/feedback.php',
+				url: 'mailer/contact-form-handler.php',
 				type: 'post',
 				dataType: 'json',
 				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&subject='+ $("#cform").find('input[name="subject"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
